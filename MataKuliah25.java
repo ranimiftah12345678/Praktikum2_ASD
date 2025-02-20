0cb469b940887edc8167e589ae096be4d667057b
ranimiftah12345678 <ranimiptahhh@gmail.com>
2025-02-20 23:05:59 +0700

    praktikum 2

---
+++ MataKuliah25.java
@@ -0,0 +1,15 @@
+public class MataKuliah25 {
+    public static void main(String[] args) {
+        
+    }
+    String kodeMk;
+    String nama;
+    int sks;
+    int jumlahJam;
+}
+public void tampilkanInformasi() {
+    System.out.println("kodeMk: " + kodeMk);
+    System.out.println("nama: " + nama)
+    System.out.println("sks: " + sks);
+    System.out.println("jumlahJam: " + jumlahJam);
+}
